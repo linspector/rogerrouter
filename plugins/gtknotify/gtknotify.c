8fc60d7cc5349f743911e74df74afdfe7d6c170d
Jan-Michael Brummer <jan.brummer@tabos.org>
2019-02-02 18:23:14 +0100

    gtknotify: Move window creation to idle loop and add it as application window
    Fixes window/app crashing on incoming call

--- plugins/gtknotify/gtknotify.c
+++ plugins/gtknotify/gtknotify.c
@@ -28,42 +28,54 @@
 
 G_GNUC_BEGIN_IGNORE_DEPRECATIONS
 
+static guint uid = 0;
+static GHashTable *hash_table = NULL;
+
+typedef struct {
+  RmConnection *connection;
+  RmContact *contact;
+  guint uid;
+} GtkNotifyData;
+
 /**
  * gtknotify_close:
- * @priv: pointer to notification window
+ * @priv: pointer to notification data
  *
  * Close notification window
  */
 void gtknotify_close(gpointer priv)
 {
-	gtk_widget_destroy(priv);
+  GtkNotifyData *data = priv;
+  GtkWidget *window = g_hash_table_lookup (hash_table, data);
+
+  if (window != NULL) {
+	  gtk_widget_destroy (window);
+    g_hash_table_remove (hash_table, data);
+  }
 }
 
 /**
  * gtknotify_timeout_close_cb:
- * @window: pointer to active notification window
+ * @user_data: gtk notifcation data pointer
  *
- * Closes given @window
+ * Closes given window identified by @uid
  *
  * Returns: %G_SOURCE_REMOVE
  */
-static gboolean gtknotify_timeout_close_cb(gpointer window)
+static gboolean gtknotify_timeout_close_cb(gpointer user_data)
 {
-	gtknotify_close(window);
+  GtkNotifyData *data = user_data;
+
+	gtknotify_close(data);
 
 	return G_SOURCE_REMOVE;
 }
 
-/**
- * gtknotify_show:
- * @connection: a #RmConnection
- * @contact: a #RmContact
- *
- * Shows a native gtk window with call details of @connection and @contact
- * Returns: %NULL
- */
-gpointer gtknotify_show(RmConnection *connection, RmContact *contact)
+static gboolean gtknotify_show_idle (gpointer user_data)
 {
+  GtkNotifyData *data = (GtkNotifyData *) user_data;
+  RmConnection *connection = data->connection;
+  RmContact *contact = data->contact;
 	GtkWidget *window;
 	GtkWidget *headerbar;
 	GtkWidget *contact_company_label;
@@ -72,13 +84,15 @@ gpointer gtknotify_show(RmConnection *connection, RmContact *contact)
 	GtkWidget *contact_street_label;
 	GtkWidget *contact_city_label;
 	GtkWidget *image;
-	gchar *tmp;
+	g_autofree gchar *tmp = NULL;
 	GtkBuilder *builder;
+  GtkApplication *app = GTK_APPLICATION(g_application_get_default());
 
 	builder = gtk_builder_new_from_resource("/org/tabos/roger/plugins/gtknotify/gtknotify.glade");
 	if (!builder) {
 		g_warning("Could not load gtknotify ui");
-		return NULL;
+    g_free (data);
+		return G_SOURCE_REMOVE;
 	}
 
 	window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
@@ -93,7 +107,6 @@ gpointer gtknotify_show(RmConnection *connection, RmContact *contact)
 
 	tmp = connection->local_number ? g_strdup_printf(_("(on %s)"), connection->local_number) : g_strdup(_("(on ?)"));
 	gtk_header_bar_set_subtitle(GTK_HEADER_BAR(headerbar), tmp);
-	g_free(tmp);
 
 	gtk_label_set_text(GTK_LABEL(contact_name_label), contact->name ? contact->name : "");
 	gtk_label_set_text(GTK_LABEL(contact_number_label), contact->number ? contact->number : "");
@@ -101,7 +114,6 @@ gpointer gtknotify_show(RmConnection *connection, RmContact *contact)
 	gtk_label_set_text(GTK_LABEL(contact_street_label), contact->street ? contact->street : "");
 	tmp = g_strdup_printf("%s%s%s", contact->zip ? contact->zip : "", contact->zip ? " " : "", contact->city ? contact->city : "");
 	gtk_label_set_text(GTK_LABEL(contact_city_label), tmp);
-	g_free(tmp);
 
 	if (contact->image) {
 		GdkPixbuf *buf = rm_image_scale(contact->image, 96);
@@ -134,14 +146,39 @@ gpointer gtknotify_show(RmConnection *connection, RmContact *contact)
 
 		gtk_header_bar_set_title(GTK_HEADER_BAR(headerbar), _("Outgoing call"));
 
-		g_timeout_add_seconds(duration, gtknotify_timeout_close_cb, window);
+		g_timeout_add_seconds(duration, gtknotify_timeout_close_cb, data);
 	}
 
 	gtk_window_set_gravity (GTK_WINDOW(window), GDK_GRAVITY_SOUTH_EAST);
 	gtk_window_move(GTK_WINDOW(window), gdk_screen_width(), gdk_screen_height());
 	gtk_widget_show(window);
 
-	return window;
+  gtk_application_add_window (app, GTK_WINDOW (window));
+
+  g_hash_table_insert (hash_table, data, window);
+
+  return G_SOURCE_REMOVE;
+}
+
+/**
+ * gtknotify_show:
+ * @connection: a #RmConnection
+ * @contact: a #RmContact
+ *
+ * Shows a native gtk window with call details of @connection and @contact
+ * Returns: %NULL
+ */
+gpointer gtknotify_show(RmConnection *connection, RmContact *contact)
+{
+  GtkNotifyData *data = g_new (GtkNotifyData, 1);
+
+  data->connection = connection;
+  data->contact = contact;
+  data->uid = ++uid;
+
+  g_idle_add_full (G_PRIORITY_DEFAULT_IDLE, gtknotify_show_idle, data, NULL);
+
+  return data;
 }
 
 /**
@@ -174,7 +211,7 @@ RmNotification gtknotify = {
 gboolean gtknotify_plugin_init(RmPlugin *plugin)
 {
 	rm_notification_register(&gtknotify);
-
+  hash_table = g_hash_table_new (NULL, NULL);
 	return TRUE;
 }
 
@@ -188,6 +225,7 @@ gboolean gtknotify_plugin_init(RmPlugin *plugin)
  */
 gboolean gtknotify_plugin_shutdown(RmPlugin *plugin)
 {
+  g_clear_object (&hash_table);
 	rm_notification_unregister(&gtknotify);
 
 	return TRUE;
