c25ffc4f54d63af69741cba7c38f9ad202b5a3eb
Jan-Michael Brummer <jan.brummer@tabos.org>
2019-09-05 13:27:42 +0200

    Windows: Update spandsp patch

--- platform/windows/mxe-pkgs/spandsp.mk
+++ platform/windows/mxe-pkgs/spandsp.mk
@@ -1,16 +1,19 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
 PKG             := spandsp
 $(PKG)_WEBSITE  := https://www.soft-switch.org
-$(PKG)_DESCR    := SpanDSP
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.0.6pre21
-$(PKG)_CHECKSUM := bd152152bf0b204661ab9439c5a649098bcb8cefebcbfa959dd602442739aa50
+$(PKG)_VERSION  := 0.0.6
+$(PKG)_CHECKSUM := cc053ac67e8ac4bb992f258fd94f275a7872df959f6a87763965feabfdcc9465
 $(PKG)_SUBDIR   := spandsp-0.0.6
-$(PKG)_FILE     := spandsp-$($(PKG)_VERSION).tgz
-$(PKG)_URL      := https://www.soft-switch.org/downloads/spandsp/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc tiff
+$(PKG)_FILE     := spandsp-$($(PKG)_VERSION).tar.gz
+# Due to frequent downtime of soft-switch.org several project have created mirrors
+# Use gstreamer mirror as it is the most stable one
+$(PKG)_URL      := https://gstreamer.freedesktop.org/src/mirror/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc tiff
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://www.soft-switch.org/downloads/spandsp/' | \
+    $(WGET) -q -O- 'https://download.soft-switch.org/downloads/spandsp/' | \
     grep '<a href="spandsp-' | \
     $(SED)  -n "s,.*spandsp-\([0-9]\+\.[0-9]*[02468]\.[^']*\)\.tgz.*,\1,p" | \
     $(SORT) -Vr |\
@@ -18,9 +21,9 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure \
-	$(subst docdir$(comma),,$(MXE_CONFIGURE_OPTS)) \
+    cd '$(1)' && autoreconf -i && ./configure \
+    $(subst docdir$(comma),,$(MXE_CONFIGURE_OPTS)) \
         LIBS="`'$(TARGET)-pkg-config' --libs libtiff-4`"
-    $(MAKE) -C '$(1)'
-    $(MAKE) -C '$(1)' install
+    $(MAKE) -C '$(1)' -j '$(JOBS)' $(MXE_DISABLE_CRUFT)
+    $(MAKE) -C '$(1)' -j 1 install $(MXE_DISABLE_CRUFT)
 endef
