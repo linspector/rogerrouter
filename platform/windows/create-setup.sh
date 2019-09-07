e9367eb4dbe0b1652081b6b0d3aca1e26782a60c
Jan-Michael Brummer <jan.brummer@tabos.org>
2019-09-07 22:57:41 +0200

    CI: Add windows build

--- platform/windows/create-setup.sh
+++ platform/windows/create-setup.sh
@@ -2,8 +2,6 @@ MXE=$(pwd)/mxe
 MXE_SYSROOT=$MXE/usr/i686-w64-mingw32.shared.posix
 
 echo Creating setup with files from $MXE_SYSROOT .
-cp build/platform/windows/roger.nsi $MXE_SYSROOT
-cp printer.nsh $MXE_SYSROOT
-cp settings.ini $MXE_SYSROOT
 
-makensis $MXE_SYSROOT/roger.nsi
+cd ../../
+makensis -dMXE_SYSROOT=$MXE_SYSROOT -DPREFIX=$MXE_SYSROOT platform/windows/build/platform/windows/roger.nsi
