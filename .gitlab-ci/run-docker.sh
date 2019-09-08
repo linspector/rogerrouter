e17353326ac88ce02e98f8243a0e5ce796ddee40
Jan-Michael Brummer <jan.brummer@tabos.org>
2019-09-08 21:26:58 +0200

    CI: Add windows ci image docker files

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -4,6 +4,10 @@ set -e
 
 TAG="registry.gitlab.com/tabos/rogerrouter/master:v1"
 
+rm -rf mxe
+mkdir mxe
+cp ../platform/windows/mxe-pkgs/* mxe
+
 cd "$(dirname "$0")"
 docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
@@ -16,3 +20,5 @@ else
     --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
     --tty --interactive "${TAG}" bash
 fi
+
+rm -rf mxe
