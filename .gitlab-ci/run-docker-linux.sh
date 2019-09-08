57fe8480758254b00a1ded4f0807329eedc3952c
Jan-Michael Brummer <jan.brummer@tabos.org>
2019-09-08 22:11:05 +0200

    CI: Use new docker image and add files for linux docker image

---
+++ .gitlab-ci/run-docker-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+TAG="registry.gitlab.com/tabos/rogerrouter/linux:v1"
+
+cd "$(dirname "$0")"
+docker build --tag "${TAG}" --file "Dockerfile.linux" .
+
+if [ "$1" = "--push" ]; then
+  docker login registry.gitlab.com
+  docker push $TAG
+else
+  docker run --rm --security-opt label=disable \
+    --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
+    --tty --interactive "${TAG}" bash
+fi
