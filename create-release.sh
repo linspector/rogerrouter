19dc942876667d3f2113ea1fb16d3191da9ff36b
Jan-Michael Brummer <jan.brummer@tabos.org>
2021-02-20 22:00:10 +0100

    Remove obsolete files

--- create-release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-NAME=`grep -Po "project\('\K.*?(?=')" meson.build`
-VERSION=`grep -Po "version : '\K.*?(?=')" meson.build`
-
-git archive --prefix=${NAME}-${VERSION}/ -o ${NAME}_${VERSION}.orig.tar.xz HEAD
