cc31245d1b8fbc2d60b97362bf87c09bf91c3992
Jan-Michael Brummer <jan.brummer@tabos.org>
2021-03-21 22:16:13 +0100

    windows: Add msys build script

---
+++ build-aux/windows/build-msys.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+export MSYS2_ARCH="x86_64"
+
+# Update everything
+pacman --noconfirm -Suy
+
+# Install the required packages
+pacman --noconfirm -S --needed \
+    base-devel \
+    git \
+    mingw-w64-$MSYS2_ARCH-toolchain \
+	mingw-w64-$MSYS2_ARCH-meson \
+	mingw-w64-$MSYS2_ARCH-gtk3 \
+	mingw-w64-$MSYS2_ARCH-ghostscript \
+	mingw-w64-$MSYS2_ARCH-libhandy \
+	mingw-w64-$MSYS2_ARCH-libsoup \
+	mingw-w64-$MSYS2_ARCH-speex \
+	mingw-w64-$MSYS2_ARCH-spandsp \
+	mingw-w64-$MSYS2_ARCH-libsndfile \
+	mingw-w64-$MSYS2_ARCH-gobject-introspection \
+	mingw-w64-$MSYS2_ARCH-dlfcn
+	
+git clone https://gitlab.gnome.org/GNOME/gssdp.git
+cd gssdp
+meson _build -Dvapi=false
+ninja -C _build
+ninja -C _build install
+cd ..
+
+git clone https://gitlab.gnome.org/GNOME/gupnp.git
+cd gupnp
+meson _build -Dvapi=false
+ninja -C _build
+ninja -C _build install
+cd ..
+
+git clone https://gitlab.com/tabos/rogerrouter.git
+cd rogerrouter
+meson _build
+ninja -C _build
