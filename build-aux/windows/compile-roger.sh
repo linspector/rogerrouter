a098169b569d6be3a19ec21d0d76e43f7622c5d8
Jan-Michael Brummer <jan.brummer@tabos.org>
2021-03-22 15:48:13 +0100

    Update windows build environment

--- build-aux/windows/compile-roger.sh
+++
@@ -1,7 +0,0 @@
-MXE=$(pwd)/mxe
-export PATH=$MXE/usr/bin:$PATH
-meson ../.. build --cross-file=mingw-cross.txt --prefix=$MXE/usr/i686-w64-mingw32.shared.posix --libdir=lib
-cd build
-ninja
-ninja install
-cd ..
--- build-aux/windows/create-setup.sh
+++
@@ -1,7 +0,0 @@
-MXE=$(pwd)/mxe
-MXE_SYSROOT=$MXE/usr/i686-w64-mingw32.shared.posix
-
-echo Creating setup with files from $MXE_SYSROOT .
-
-cd ../../
-makensis -dMXE_SYSROOT=$MXE_SYSROOT -DPREFIX=$MXE_SYSROOT platform/windows/build/platform/windows/roger.nsi
